60d61f96f961ffa4f9bf8c4688158227bd8a29ea
skrulen <77325542+skrulen@users.noreply.github.com>
2022-12-02 14:54:06 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <p>Яна Цист</p>
+</body>
+<scrtipt src="script.js"></scrtipt>
+</html>
